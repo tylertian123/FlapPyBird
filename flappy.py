14833816a7e2b6c31e8aae56fdf29625d71922d4
Tyler Tian <tylertian123@gmail.com>
2019-04-28 17:57:36 -0400

    Increase pipe gap

--- flappy.py
+++ flappy.py
@@ -9,7 +9,7 @@ from pygame.locals import *
 FPS = 30
 SCREENWIDTH  = 288
 SCREENHEIGHT = 512
-PIPEGAPSIZE  = 100 # gap between upper and lower part of pipe
+PIPEGAPSIZE  = 175 # gap between upper and lower part of pipe
 BASEY        = SCREENHEIGHT * 0.79
 # image, sound and hitmask  dicts
 IMAGES, SOUNDS, HITMASKS = {}, {}, {}
